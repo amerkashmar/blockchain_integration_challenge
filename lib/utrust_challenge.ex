439dcf5a2253d1e90afb84bc76ded2de9ab74bc8
Amer <amer.kashmar@gmail.com>
2022-06-10 16:21:26 +0400

    Update gitignore

--- lib/utrust_challenge.ex
+++ lib/utrust_challenge.ex
@@ -3,8 +3,12 @@ defmodule UtrustChallenge do
 
 
   def get_eth_balance(eth_address) do
-    balance = float_to_string(100.00, decimals: 2)
-    "Balance of 0x" <> eth_address <> ": " <> balance <> " ETH"
+    # parameters = [ "0x3b3Cc76c61c4d20f362E992D0024d6d22be7df71", "latest" ]
+    # :rpc.call("HTTP://127.0.0.1:7545","2.0.0", "eth_get_balance", parameters)
+
+    eth_address
+    # {_response_code, balance} = Ethereumex.HttpClient.eth_get_balance(eth_address)
+    # "Balance of 0x" <> eth_address <> ": " <> balance <> " ETH"
   end
 
   def execute_eth_transaction do
@@ -23,7 +27,4 @@ defmodule UtrustChallenge do
     "Egld transaction details"
   end
 
-  def float_to_string(float, options) do
-    :erlang.float_to_binary(float, options)
-  end
 end
