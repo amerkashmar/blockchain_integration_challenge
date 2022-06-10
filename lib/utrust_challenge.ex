b83fcfb0c2db70465a5a2c80bc03326ac3b280f9
Amer Kachmar <amer.kashmar@gmail.com>
2022-06-10 13:17:28 +0400

    initial commit

---
+++ lib/utrust_challenge.ex
@@ -0,0 +1,18 @@
+defmodule UtrustChallenge do
+  @moduledoc """
+  Documentation for `UtrustChallenge`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> UtrustChallenge.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/utrust_challenge_test.exs
@@ -0,0 +1,8 @@
+defmodule UtrustChallengeTest do
+  use ExUnit.Case
+  doctest UtrustChallenge
+
+  test "greets the world" do
+    assert UtrustChallenge.hello() == :world
+  end
+end
