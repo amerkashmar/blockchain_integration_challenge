dbeeff45991645a4feb9032607f4c6ec109d4cce
Amer <amer.kashmar@gmail.com>
2022-06-11 17:26:26 +0400

    Completed getting eth transaction details

--- config/config.exs
+++ config/config.exs
@@ -1,4 +1,6 @@
 import Config
 
 config :utrust_challenge,
-  rpc_server: "http://localhost:7545"
+  eth_rpc_server: "http://localhost:7545",
+  public_key: "0x3b3Cc76c61c4d20f362E992D0024d6d22be7df71",
+  private_key: "e23554ff12b0bb4d0b9d7fe3068d7f5db173b0ffe6b7889aa3c848c54cce4a2c"
--- lib/utrust_challenge.ex
+++ lib/utrust_challenge.ex
@@ -10,6 +10,36 @@ defmodule UtrustChallenge do
     wei_to_eth(hex_to_decimal(result))
   end
 
+  def emet_execute_my_eth_transaction(to_address, value) do
+    parameters = [%{"from" => my_config(:public_key), "to" => to_address, "value" => decimal_to_hex(value)}]
+    encoded_message = create_message("eth_sendTransaction", parameters)
+    get_http_result(encoded_message)
+  end
+
+  def eet_execute_eth_transaction(from_address, to_address, value) do
+    parameters = [%{"from" => from_address, "to" => to_address, "value" => decimal_to_hex(value)}]
+    encoded_message = create_message("eth_sendTransaction", parameters)
+    get_http_result(encoded_message)
+  end
+
+  def gtd_get_transaction_details(transaction_hash) do
+    parameters = [transaction_hash]
+    encoded_message = create_message("eth_getTransactionByHash", parameters)
+    result = get_http_result(encoded_message)
+
+    amount_in_eth = Float.to_string(wei_to_eth(hex_to_decimal(result["value"])))
+    fee_in_eth = Float.to_string(wei_to_eth(hex_to_decimal(result["gas"])))
+
+    IO.puts("\n"
+      <> "From: \t" <> result["from"] <> "\n"
+      <> "To: \t" <> result["to"] <> "\n"
+      <> "Amount: " <> amount_in_eth <> " ETH\n"
+      <> "Fee: \t" <> fee_in_eth <> " ETH\n"
+      <> "Hash: \t" <> result["hash"] <> "\n")
+
+    "Completed successfully"
+  end
+
   def hex_to_decimal(hex_number) do
     hex_sliced = hex_number |> String.slice(2..-1)
 
@@ -20,12 +50,20 @@ defmodule UtrustChallenge do
     end
   end
 
-  def wei_to_eth(wei_number) do
-    wei_number/1.0e18
+  def decimal_to_hex(decimal_number) do
+    "0x" <> Integer.to_string(eth_to_wei(decimal_number), 16)
+  end
+
+  def wei_to_eth(wei_amount) do
+    wei_amount/1.0e18
+  end
+
+  def eth_to_wei(eth_amount) do
+    trunc(eth_amount*1.0e18)
   end
 
   def get_http_result(encoded_message) do
-    url = "http://localhost:7545"
+    url = my_config(:eth_rpc_server)
 
     {:ok,response} = HTTPoison.post(url, encoded_message)
     {:ok,result} = Jason.decode(response.body)
@@ -34,15 +72,21 @@ defmodule UtrustChallenge do
 
   def create_message(method_name,parameters) do
     message = %{
+      jsonrpc: "2.0",
       method: method_name,
       params: parameters,
-      jsonrpc: "2.0",
       id: 1
     }
 
     {:ok, encoded_message} = Jason.encode(message)
 
+    # IO.puts("Message request: " <> encoded_message)
     encoded_message
   end
+
+  def my_config(config_name) do
+    Application.get_env(:utrust_challenge, config_name)
+  end
+
 
 end
